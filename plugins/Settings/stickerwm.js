ddb41361115b0b525dc1c2ae221a796838fa4f2a
xh_clinton <152104512+xhclintohn@users.noreply.github.com>
2026-06-23 04:11:36 +0300

    sync: plugins/Settings/stickerwm.js

--- plugins/Settings/stickerwm.js
+++ plugins/Settings/stickerwm.js
@@ -12,22 +12,26 @@ export default async (context) => {
         if (!settings) {
             await client.sendMessage(m.chat, { react: { text: '⌛', key: m.reactKey } });
             await client.sendMessage(m.chat, { react: { text: '❌', key: m.reactKey } }).catch(() => {});
-            return await sendInteractive(client, m, "│ Settings not found. Something's seriously broken.\n╰───────────────\n> ©𝐏𝐨𝐰𝐞𝐫𝐞𝐝 𝐁𝐲 𝐱𝐡_𝐜𝐥𝐢𝐧𝐭𝐨𝐧");
+            return await sendInteractive(client, m, `╭─❏ 「 STICKER WM 」
+│ Settings not found. Something's seriously broken.\n╰───────────────\n> ©𝐏𝐨𝐰𝐞𝐫𝐞𝐝 𝐁𝐲 𝐱𝐡_𝐜𝐥𝐢𝐧𝐭𝐨𝐧`);
         }
 
         if (newStickerWM !== null) {
             if (newStickerWM === 'null') {
                 if (!settings.packname) {
                     await client.sendMessage(m.chat, { react: { text: '❌', key: m.reactKey } }).catch(() => {});
-                    return await sendInteractive(client, m, "│ Bot already has no sticker watermark, genius.\n╰───────────────\n> ©𝐏𝐨𝐰𝐞𝐫𝐞𝐝 𝐁𝐲 𝐱𝐡_𝐜𝐥𝐢𝐧𝐭𝐨𝐧");
+                    return await sendInteractive(client, m, `╭─❏ 「 STICKER WM 」
+│ Bot already has no sticker watermark, genius.\n╰───────────────\n> ©𝐏𝐨𝐰𝐞𝐫𝐞𝐝 𝐁𝐲 𝐱𝐡_𝐜𝐥𝐢𝐧𝐭𝐨𝐧`);
                 }
                 await updateSetting('packname', '');
                 await client.sendMessage(m.chat, { react: { text: '✅', key: m.reactKey } });
-                await sendInteractive(client, m, "│ Sticker watermark removed. Happy now?\n╰───────────────\n> ©𝐏𝐨𝐰𝐞𝐫𝐞𝐝 𝐁𝐲 𝐱𝐡_𝐜𝐥𝐢𝐧𝐭𝐨𝐧");
+                await sendInteractive(client, m, `╭─❏ 「 STICKER WM 」
+│ Sticker watermark removed. Happy now?\n╰───────────────\n> ©𝐏𝐨𝐰𝐞𝐫𝐞𝐝 𝐁𝐲 𝐱𝐡_𝐜𝐥𝐢𝐧𝐭𝐨𝐧`);
             } else {
                 if (settings.packname === newStickerWM) {
                     await client.sendMessage(m.chat, { react: { text: '❌', key: m.reactKey } }).catch(() => {});
-                    return await sendInteractive(client, m, `│ Watermark already set to: ${newStickerWM}\n│ Stop wasting my time.\n╰───────────────\n> ©𝐏𝐨𝐰𝐞𝐫𝐞𝐝 𝐁𝐲 𝐱𝐡_𝐜𝐥𝐢𝐧𝐭𝐨𝐧`);
+                    return await sendInteractive(client, m, `╭─❏ 「 STICKER WM 」
+│ Watermark already set to: ${newStickerWM}\n│ Stop wasting my time.\n╰───────────────\n> ©𝐏𝐨𝐰𝐞𝐫𝐞𝐝 𝐁𝐲 𝐱𝐡_𝐜𝐥𝐢𝐧𝐭𝐨𝐧`);
                 }
                 await updateSetting('packname', newStickerWM);
                 await client.sendMessage(m.chat, { react: { text: '✅', key: m.reactKey } });

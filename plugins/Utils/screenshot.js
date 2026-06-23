d3d4cf6940c9e3eefc1801db7749c4eed28c3db6
xh_clinton <152104512+xhclintohn@users.noreply.github.com>
2026-06-23 04:11:46 +0300

    sync: plugins/Utils/screenshot.js

--- plugins/Utils/screenshot.js
+++ plugins/Utils/screenshot.js
@@ -12,10 +12,12 @@ let cap = `╭─❏ 「 SCREENSHOT」
 
 if (!text) {
     await client.sendMessage(m.chat, { react: { text: '❌', key: m.reactKey } }).catch(() => {});
-    return sendInteractive(client, m, "│ Provide a website link to screenshot, moron.\n╰───────────────\n> ©𝐏𝐨𝐰𝐞𝐫𝐞𝐝 𝐁𝐲 𝐱𝐡_𝐜𝐥𝐢𝐧𝐭𝐨𝐧")
+    return sendInteractive(client, m, `╭─❏ 「 USAGE 」
+│ Provide a website link to screenshot, moron.\n╰───────────────\n> ©𝐏𝐨𝐰𝐞𝐫𝐞𝐝 𝐁𝐲 𝐱𝐡_𝐜𝐥𝐢𝐧𝐭𝐨𝐧`)
 }
 
-const image = `https://image.thum.io/get/fullpage/${text}`
+const url = /^https?:\/\//i.test(text) ? text : `https://${text}`;
+const image = `https://image.thum.io/get/fullpage/${url}`
 
 await client.sendMessage(m.chat, { image: { url: image }, caption: cap});
 
@@ -23,7 +25,8 @@ await client.sendMessage(m.chat, { image: { url: image }, caption: cap});
 } catch (error) {
     await client.sendMessage(m.chat, { react: { text: '❌', key: m.reactKey } }).catch(() => {});
 
-sendInteractive(client, m, "│ Screenshot failed. Probably your garbage link.\n╰───────────────\n> ©𝐏𝐨𝐰𝐞𝐫𝐞𝐝 𝐁𝐲 𝐱𝐡_𝐜𝐥𝐢𝐧𝐭𝐨𝐧")
+sendInteractive(client, m, `╭─❏ 「 ERROR 」
+│ Screenshot failed. Probably your garbage link.\n╰───────────────\n> ©𝐏𝐨𝐰𝐞𝐫𝐞𝐝 𝐁𝐲 𝐱𝐡_𝐜𝐥𝐢𝐧𝐭𝐨𝐧`)
 
 }
 
